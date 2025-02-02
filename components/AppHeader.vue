ff63ce5c0f6122d73ccce7193974560852a6db9a
alv1k <alekseevaalena442@gmail.com>
2025-02-02 20:10:52 +0900

    test

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,6 +4,7 @@
         <ul>
           <!-- <li><NuxtLink to="/about">About</NuxtLink></li> -->
           <li><NuxtLink to="/pages/Utilities.vue">Post 1</NuxtLink></li>
+          <li><NuxtLink to="/pages/Utilities.vue">Post 2</NuxtLink></li>
           <!-- <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li> -->
         </ul>
       </nav>
